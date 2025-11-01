e4f98da36987e5cdeea15cb10afb36e9a5a979f8
Site <nathsivleg@icloud.com>
2025-11-01 21:37:17 +0100

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// config.js  <-- GARDER LOCAL uniquement, ne pas partager, ne pas committer
-const OPENAI_KEY = "sk-projdCqC7gyivPATBEn4RTRmBobm13KDzZvhuzclt_mPUNFVdn7rvtb2nCSqjFRZNSEwy4pWJyFIO4T3BlbkFJVj-tzH91osDmURhANx8HsEmtiKUX3a2mj9DDcmwoMNQXrFdQdIM8D5GE4EOzIkJhwN9E-fqsA";
-export { OPENAI_KEY };
